05867f971c603293e4332b612329c6eaed16092d
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-06-30 16:12:00 -0500

    add basic setup of the package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+name='mlproject01',
+version='0.0.1',
+author='Sanghyuk',
+author_email='sanghyuk.park85@gmail.com',
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn']
+)
