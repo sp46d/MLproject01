2e4dc951d58490392a19aa91c3a0c1cc2f9a5e82
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-06-30 22:34:10 -0500

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from setuptools import find_packages, setup
+from typing import List
 
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path, 'r') as f:
+        requirements = f.read().splitlines()
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
 
 setup(
 name='mlproject01',
@@ -7,5 +21,5 @@ version='0.0.1',
 author='Sanghyuk',
 author_email='sanghyuk.park85@gmail.com',
 packages=find_packages(),
-install_requires=['pandas', 'numpy', 'seaborn']
+install_requires=get_requirements('requirements.txt')
 )
